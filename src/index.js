7ffbeb290c2643bfe74eb316615e7121d5b43565
Ashish Ranjan <akp9801@gmail.com>
2022-12-20 19:40:26 +0530

    createRoot() is implemented with render function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root")) // createRoot() function
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// this is render function without the createroot function.
+// ReactDOM.render(
+//  <h1>Welcome to my webapp</h1>,
+//  document.getElementById("root")
+// )
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <h1>Welcome to my webapp</h1>
+)
